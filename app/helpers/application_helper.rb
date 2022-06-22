5d67c620f26187488621e87df675d613ae0f6390
Andrew Vogel <andrewpvogel@gmail.com>
2022-06-22 14:34:55 -0400

    feat: add color status indicators to listing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def listing_status_indicator(floor_plan)
+    status_classes = %w[flex flex-col h-100 rounded-sm mr-4 w-2]
+
+    case floor_plan.listing_status
+    when :unavailable
+      status_classes.push('bg-red-400')
+    when :new
+      status_classes.push('bg-green-400')
+    else
+      status_classes.push('bg-blue-400')
+    end
+
+    content_tag('div', '', class: status_classes.join(' '))
+  end
 end
--- app/models/floor_plan.rb
+++ app/models/floor_plan.rb
@@ -2,4 +2,14 @@ class FloorPlan < ApplicationRecord
   validates :name, :community, presence: true
   validates :name, uniqueness: { scope: :community }
   validates :baths, :beds, :price, :sq_ft, numericality: { only_integer: true }
+
+  def listing_status
+    if updated_at <= 12.hours.ago
+      :unavailable
+    elsif updated_at == created_at
+      :new
+    else
+      :available
+    end
+  end
 end
