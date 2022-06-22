5e6e7360063561a20db1252dc60417df4bd67d3f
Andrew Vogel <andrewpvogel@gmail.com>
2022-06-21 21:14:06 -0400

    fix: typo on floor_plan validation

--- app/models/floor_plan.rb
+++ app/models/floor_plan.rb
@@ -1,5 +1,5 @@
 class FloorPlan < ApplicationRecord
   validates :name, :community, presence: true
   validates :name, uniqueness: { scope: :community }
-  validates :baths, :beds, :price, :sq_ft, numericality: true, only_integer: { true }
+  validates :baths, :beds, :price, :sq_ft, numericality: { only_integer: true }
 end
