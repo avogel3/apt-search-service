ed67d2be4fce732d5c4b8bc7693ebe8a7b0f295a
Andrew Vogel <andrewpvogel@gmail.com>
2022-06-27 13:04:03 -0400

    feat: show indicator if multiple prices

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,3 +1,3 @@
 class ApartmentsController < ApplicationController
-  expose(:floor_plans) { FloorPlan.includes(:current_price_data).order(community: :desc) }
+  expose(:floor_plans) { FloorPlan.includes(:current_price_data, :price_histories).order(community: :desc) }
 end
